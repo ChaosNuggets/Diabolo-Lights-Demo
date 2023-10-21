a4e07332777d34de37cdd8bf12dd7d5837d0820c
ChaosNuggets <46007806+ChaosNuggets@users.noreply.github.com>
2023-10-21 19:23:12 -0400

    got sleep mode working
    it will sleep when the leds are off and the button is released 50ms after the debouncing.
    it will wake up and turn on the leds as soon as the button is pushed (no debounce delay)

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,40 @@ const int NUM_LEDS = 6;
 
 Adafruit_NeoPixel pixels(NUM_LEDS, LED_PIN, NEO_RGB + NEO_KHZ800);
 
-enum LED_State {OFF, RED, GREEN, BLUE, COUNT};
-LED_State led_state = OFF;
+bool are_leds_on = true;
+enum LED_State {
+    SETUP,
+    GETTING_BRIGHTER,
+    OFF,
+    DIM,
+
+};
 
 unsigned long last_debounce_time = 0;
 const int DEBOUNCE_DELAY = 50; //ms
 int debounce_button_state;
 int button_state;
+unsigned long turn_on_time = 0;
+
+ISR(PCINT0_vect) { // This should only run when it is woken up from sleep mode
+    turn_on_time = millis();
+    last_debounce_time = millis();
+    are_leds_on = true;
+    button_state = HIGH;
+
+    cli(); // disable interrupts
+    PCMSK &= ~(1 << PCINT2); // turns of PCINT2 as interrupt pin
+    sleep_disable(); // clear sleep enable bit
+}
+
+void shut_down() {
+    GIMSK |= 1 << PCIE; // enable pin change interrupt
+    PCMSK |= 1 << PCINT2; // turns on PCINT2 as interrupt pin
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+    sleep_enable(); //set sleep enable bit
+    sei(); // enable interrupts
+    sleep_mode(); // put the microcontroller to sleep
+}
 
 void setup() {
     pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
@@ -33,22 +60,19 @@ void loop() {
         debounce_button_state = reading;
     }
 
-    if ((millis() - last_debounce_time) > DEBOUNCE_DELAY) {
-        if (reading != button_state) {
-            button_state = reading;
+    if ((millis() - last_debounce_time) > DEBOUNCE_DELAY && reading != button_state) {
+        button_state = reading;
 
-            if (button_state == HIGH) {
-                led_state = static_cast<LED_State>(led_state < COUNT - 1 ? led_state + 1 : 0);
-            }
+        if (are_leds_on && button_state == HIGH) {
+            are_leds_on = false;
+        } 
+
+        if (!are_leds_on && button_state == LOW) {
+            shut_down();
         }
     }
 
-    uint32_t color
-        = led_state == OFF ? pixels.Color(0, 0, 0)
-        : led_state == RED ? pixels.Color(50, 0, 0)
-        : led_state == GREEN ? pixels.Color(0, 50, 0)
-        : led_state == BLUE ? pixels.Color(0, 0, 50)
-        : pixels.Color(50, 50, 50);
+    uint32_t color = are_leds_on ? pixels.Color(50, 50, 50) : pixels.Color(0, 0, 0);
     
     for(int i = 0; i < NUM_LEDS; i++) {
         pixels.setPixelColor(i, color);
